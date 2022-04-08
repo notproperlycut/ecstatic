ec011b7690449f44fc48d471eb088b68580f9fc2
Chris Tierney <notproperlycut@gmail.com>
2022-04-08 06:58:26 +0000

    mix init --sup ecstatic

---
+++ lib/ecstatic/application.ex
@@ -0,0 +1,20 @@
+defmodule Ecstatic.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Ecstatic.Worker.start_link(arg)
+      # {Ecstatic.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Ecstatic.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

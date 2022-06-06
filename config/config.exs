6a571ba21e2bfbcf3e497032775728b4b276cf50
Chris Tierney <notproperlycut@gmail.com>
2022-06-06 08:31:38 +0000

    Configure using ecstatic config vars

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,20 @@
 import Config
 
+# TODO: why is this required, given that we're configuring the Commanded Application
+# in lib/ecstatic/commanded.ex
 config :ecstatic, event_stores: [Ecstatic.EventStore]
 
-import_config "#{config_env()}.exs"
+if Mix.env() == :test do
+  config :ecstatic, :database,
+  username: "postgres",
+  password: "postgres",
+  database: "eventstore_test",
+  hostname: "localhost",
+  pool_size: 1
+
+  config :logger, level: :warn
+
+  config :commanded,
+  assert_receive_event_timeout: 100,
+  refute_receive_event_timeout: 100
+end
--- lib/ecstatic/event_store.ex
+++ lib/ecstatic/event_store.ex
@@ -1,3 +1,8 @@
 defmodule Ecstatic.EventStore do
   use EventStore, otp_app: :ecstatic
+
+  def init(_) do
+    config = Application.fetch_env!(:ecstatic, :database) |> Keyword.put(:serializer, Commanded.Serialization.JsonSerializer)
+    {:ok, config}
+  end
 end
